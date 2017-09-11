fc49460b52dcb9cce81cb5cd67bdb9745db9543d
James Otis <jadotis@bu.edu>
2017-09-11 18:19:13 -0400

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LinkComponent } from './link/link.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LinkComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
